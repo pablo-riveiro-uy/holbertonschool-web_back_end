cdb4819342a980958dafa9fc80e907e0f2deb0c0
root <priveiro@gmail.com>
2023-08-28 11:46:52 -0700

    fixing var annotation

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     wait_time: float = uniform(0, max_delay)
     await asyncio.sleep(wait_time)
     return wait_time

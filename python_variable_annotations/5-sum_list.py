fae78b5657b03e1f4dbebf4a91070e00162cb7e8
root <priveiro@gmail.com>
2023-08-28 08:04:01 -0700

    task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" take a list of float and retuns a sum as a float """
+
+
+from typing import List
+
+def sum_list(sum_list: List[float]) -> float:
+    """ take a list of float and retuns a sum as a float """
+    return sum(sum_list)

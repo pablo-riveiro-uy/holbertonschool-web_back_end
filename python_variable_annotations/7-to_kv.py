2bd853655bd89793b43ff6826c8856ea7359d467
root <priveiro@gmail.com>
2023-08-28 09:34:19 -0700

    fixing variables names

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,7 +4,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(text: str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ takes a str, float or float and returns a tuple of value """
     result: float = v**2
-    return [text, result]
+    return [k, result]

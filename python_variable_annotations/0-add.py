45e65cdd81b28c5c39678cd432a501b82945d6d2
root <priveiro@gmail.com>
2023-08-28 06:54:20 -0700

    task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" type-annoted function add function """
+
+
+def add(a: float, b: float) -> float:
+    """ take two float numbers and retunrs the add"""
+    return a + b

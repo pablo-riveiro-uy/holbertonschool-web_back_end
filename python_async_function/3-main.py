8cdea9ab8c7d17799fed53d220021e4083c1a5ca
TheWhiteMachine <priveiro@gmail.com>
2023-08-29 10:13:08 -0700

    documentation missing fixed

--- python_async_function/3-main.py
+++ python_async_function/3-main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-
+""" create and returns a asyncio task """
 import asyncio
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def test(max_delay: int) -> float:
+    """ create and returns a asyncio task """
     task = task_wait_random(max_delay)
     await task
     print(task.__class__)

6551a49d28399b957a11991f5aa35b5dddf3d687
TheWhiteMachine <priveiro@gmail.com>
2023-08-29 12:37:52 -0700

    correct annotation for generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,11 @@
 
 import asyncio
 from random import uniform
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
+    """  yield random numbers waiting 1 second beyween """
     for i in range(10):
         await asyncio.sleep(1)
         yield uniform(0, 10)

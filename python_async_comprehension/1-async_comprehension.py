7ff5962d86502f7b93bb2b198197e3c0302d39b9
TheWhiteMachine <priveiro@gmail.com>
2023-08-29 13:00:43 -0700

    changing return type

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,11 @@
 """ collect a list of numbers asynchronously geenerated """
 import asyncio
 from typing import List
+import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List:
+async def async_comprehension() -> typing.List[float]:
     """ collect a list of numbers asynchronously geenerated """
     result = [i async for i in async_generator()]
     return result

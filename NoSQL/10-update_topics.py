79d404df631f79655f0b27ee3bee1ff8932cdd04
root <priveiro@gmail.com>
2023-09-06 06:22:48 -0700

    10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """ change topics of documentation based on the name """
 
 import pymongo
 
+
 def update_topics(mongo_collection, name, topics):
     """ change topics of documentation based on the name """
     for topic in topics:
-        mongo_collection.update({"name": name},{"$set": {"topics": topic}})
-
+        mongo_collection.update_many(
+            {"name": name}, {"$set": {"topics": topic}})

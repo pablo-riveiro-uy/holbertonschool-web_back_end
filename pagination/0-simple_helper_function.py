7096d9b3edd194a51293789eda56b2575d5ead88
TheWhiteMachine <priveiro@gmail.com>
2023-09-07 07:52:54 -0700

    refactoring

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,13 +2,9 @@
 """  a helper function to get page number and pagination limit  """
 
 
-
 def index_range(page: int, page_size: int) -> tuple:
     """  a helper function to get page number and pagination limit  """
-    first: int = 0
-    last: int = page_size
 
-    if page != 1:
-        last = page_size * page
-        first = last / page_size * 10
-    return(int(first), last)
+    last = page_size * page
+    first = (page - 1) * page_size
+    return (first, last)

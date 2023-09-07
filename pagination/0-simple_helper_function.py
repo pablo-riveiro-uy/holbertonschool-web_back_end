7c9629c021f631ebf8adb025a42fc391dec6c965
TheWhiteMachine <priveiro@gmail.com>
2023-09-07 07:15:41 -0700

    adding defect values

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -6,9 +6,10 @@ from math import trunc
 
 def index_range(page: int, page_size: int):
     """  a helper function to get page number and pagination limit  """
+    first = 0
+    last = page_size
     if page == 1:
-        first = 0
-        last = page_size
+        pass
     else:
         last = page_size * page
         first = last / page_size * 10

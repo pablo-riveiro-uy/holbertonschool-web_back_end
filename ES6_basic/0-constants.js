f1820ebcb69ce8431f601a2bdc31d8cb0b2b7c49
TheWhiteMachine <priveiro@gmail.com>
2023-09-14 10:22:36 -0700

    changing tabs for spaces

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,3 +1,4 @@
+// task 0
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -8,7 +9,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-  let combination = ' But sometimes let';
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;

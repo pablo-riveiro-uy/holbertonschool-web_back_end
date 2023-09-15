f4af014852584af5f0e9396c6dbc81f830c38dab
TheWhiteMachine <priveiro@gmail.com>
2023-09-15 10:21:06 -0700

    task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw TypeError('Students must be an array');
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter
+  get name() {
+    return this._name;
+  }
+  // Getter
+
+  get lenght() {
+    return this._length;
+  }
+  // Getter
+
+  get students() {
+    return this._students;
+  }
+
+  // Setter
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  set length(lenNum) {
+    if (typeof lenNum !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = lenNum;
+  }
+
+  set students(studentsNum) {
+    if (!Array.isArray(studentsNum)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = studentsNum;
+  }
+}

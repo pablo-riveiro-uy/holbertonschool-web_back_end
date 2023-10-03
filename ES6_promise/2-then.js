2b2b9b928cbe0d2cb9b4cc8b3d752f080ee0a8ab
TheWhiteMachine <priveiro@gmail.com>
2023-10-03 15:16:54 -0700

    trying to fix 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     if (promise) {
       resolve({ status: 200, body: 'success' });
     } else {
       return (new Error());
     }
-    reject(new Error());
+
     return (new Error());
   }).finally(console.log('Got a response from the API'));
 }

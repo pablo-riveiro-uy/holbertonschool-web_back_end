d0f7a7fa96e5436b5b5f6c081507ca45396a3743
TheWhiteMachine <priveiro@gmail.com>
2023-10-03 15:31:56 -0700

    adjusting 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     if (promise) {
       resolve({ status: 200, body: 'success' });
     } else {
-      return (new Error());
+      reject(new Error());
     }
-
-    return (new Error());
   }).finally(console.log('Got a response from the API'));
 }

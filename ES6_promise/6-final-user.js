644874fe724e42e991e4c7447ceec07a12c0fef3
TheWhiteMachine <priveiro@gmail.com>
2023-10-03 18:34:00 -0700

    test 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,18 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  let result = []
-  return new Promise((resolve) => {
-    if (resolve) {
-      Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((values) => {
-        result.push(values.resovle.status);
-        result.push(values.reject);
-        resolve(
-          result,
-        ).catch(
-          result,
-        );
-      });
-    }
-  });
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return (Promise.allSettled([signUpPromise, uploadPromise]).then(((results) => {
+    return results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }));
+  })));
 }

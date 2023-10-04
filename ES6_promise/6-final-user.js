e27dc5edbe20b997823f515e6548483dee6b9378
TheWhiteMachine <priveiro@gmail.com>
2023-10-03 18:42:27 -0700

    6 with eslint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,10 +5,11 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(fileName);
 
-  return (Promise.allSettled([signUpPromise, uploadPromise]).then(((results) => {
-    return results.map((result) => ({
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then(((results) => (results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-  })));
+    })))
+
+    ));
 }

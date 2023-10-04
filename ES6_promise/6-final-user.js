58634ddfee4e2f78160526be4f31cdbdfd0d6d8d
TheWhiteMachine <priveiro@gmail.com>
2023-10-03 17:18:59 -0700

    trying output of 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,23 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  let result = []
   return new Promise((resolve) => {
     if (resolve) {
       Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((values) => {
+        result.push(values.resovle.status);
+        result.push(values.reject);
         resolve(
-          [
-            {
-              status: values.status,
-              value: values.resolve,
-            },
-          ],
+          result,
         ).catch(
-          [
-            {
-              status: values.status,
-              value: values.reject,
-            },
-          ],
+          result,
         );
       });
     }

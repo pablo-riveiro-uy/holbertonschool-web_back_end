106e76d02eae7c0845cb4a4e884ac6be230779a3
TheWhiteMachine <priveiro@gmail.com>
2023-10-09 12:59:31 -0700

    using map on 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,36 +1,29 @@
 export default function getListStudents() {
-  const result = [];
-  const obj1 = {
-    firstName: 'Guillaume',
-    id: 1,
-    location: 'San Francisco',
-  };
-  const obj2 = {
-    firstName: 'James',
-    id: 2,
-    location: 'Columbia',
-  };
-  const obj3 = {
-    firstName: 'Serena',
-    id: 5,
-    location: 'San Francisco',
-  };
+  const Students = [
+    {
+      firstName: 'Guillaume',
+      id: 1,
+      location: 'San Francisco',
+    },
+    {
+      firstName: 'James',
+      id: 2,
+      location: 'Columbia',
+    },
+    {
+      firstName: 'Serena',
+      id: 5,
+      location: 'San Francisco',
+    },
+  ];
 
-  result.push({
-    id: obj1.id,
-    firstName: obj1.firstName,
-    location: obj1.location,
-  });
-  result.push({
-    id: obj2.id,
-    firstName: obj2.firstName,
-    location: obj2.location,
-  });
-  result.push({
-    id: obj3.id,
-    firstName: obj3.firstName,
-    location: obj3.location,
-  });
+  let result = [];
+
+  result = Students.map((obj) => ({
+    id: obj.id,
+    firtName: obj.firstName,
+    location: obj.location,
+  }));
 
   return result;
 }

9717a1ee0ee8d59e6017d4a9fe947701136c85a3
TheWhiteMachine <priveiro@gmail.com>
2023-10-09 09:06:37 -0700

    task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+
+	const obj1 = {
+		id: 1,
+		FirstName: 'Guillermo',
+		location: 'San Francisco'
+	}
+	const obj2 = {
+		id: 2,
+		FirstName: 'James',
+		location: 'Colombia'
+	}
+	const obj3 = {
+		id: 5,
+		FirstName: 'Serena',
+		location: 'San Francisco'
+	}
+	const result = [obj1, obj2, obj3];
+
+	return result;
+}
